4300b3036b9f85a643d5218bbc131ce7434dddb1
programmeramit <107116182+programmeramit@users.noreply.github.com>
2026-04-05 20:36:45 +0530

    adde dthe prisma

--- actions/patient.ts
+++ actions/patient.ts
@@ -1,15 +1,20 @@
 "use server"
 
-import { number } from "framer-motion"
 import {prisma} from "../lib/prisma"
 
 export async function createPatients(data:{
     name:string,
     phone:string
 }){
-    return await prisma.patient.create({
-        data:data
-    })
+   await prisma.patient.create({
+  data: {
+    name: data.name,
+    phone: data.phone,
+    address: "Some address",
+    dateOfBirth: new Date("2000-01-01"),
+    Gender: "MALE", // or whatever enum/value you defined
+  }
+})
 }
 
 export async function getDetails(id:string){
