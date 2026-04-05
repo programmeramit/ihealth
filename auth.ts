f7c6b66f47ae25f7f408f2a5630ec8565b1f1d90
programmeramit <107116182+programmeramit@users.noreply.github.com>
2026-04-05 22:11:03 +0530

    fixed the types  in the app

--- auth.ts
+++ auth.ts
@@ -20,7 +20,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     async signIn({account,profile}){
       if(account?.provider === "google"){
         console.log(account,profile)
-        return profile?.email_verified && profile.email?.endsWith('@gmail.com')
+        return !!(profile?.email_verified && profile.email?.endsWith('@gmail.com'))
       }
       return true
     },
