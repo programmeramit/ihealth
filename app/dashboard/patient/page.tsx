2c4a36b9a917b757fbe940d4e36eeede8dbde5fb
programmeramit <107116182+programmeramit@users.noreply.github.com>
2026-04-07 14:07:37 +0530

    added the patient scheama and doctor schema

--- app/dashboard/patient/page.tsx
+++ app/dashboard/patient/page.tsx
@@ -1,33 +1,40 @@
-import { coloumns,Patients } from "./coloumns";
+import { coloumns, Patients } from "./coloumns";
 import { DataTable } from "./dataTable";
 import { prisma } from "@/lib/prisma";
-async function getPatients():Promise<Patients[]>{
-    const data = await prisma.patient.findMany({
-        select:{
-            name:true,
-            id:true,
-            type:true
-        }
-    })
+async function getPatients(): Promise<any[]> {
+  const data = await prisma.patient.findMany({
+    select: {
+      name: true,
+      id: true,
+      type: true,
+      Gender: true,
+      dateOfBirth: true,
+      appointments: {
+        select: {
+          doctor: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  console.log(data);
 
-    return data
+  return data;
 }
 
 export default async function page() {
+  const data = await getPatients();
+  return (
+    <div className="w-full h-screen">
+      <div className="">search bar</div>
 
-    const data = await getPatients()
-    return (
-        <div className="w-full h-screen">
-        <div className="">
-            search bar
-
-        </div>
-
-        <div className="mx-10">
-                  <DataTable columns={coloumns} data={data} />
-
-        </div>
-        </div>
-    )
-    
+      <div className="mx-10">
+        <DataTable columns={coloumns} data={data} />
+      </div>
+    </div>
+  );
 }
